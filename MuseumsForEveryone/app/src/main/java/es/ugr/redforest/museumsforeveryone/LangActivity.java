a231c89d0a7dd61c5a2bd566b0a369d37cb4f4c1
HackBeam <gregoaplweb@gmail.com>
2016-10-24 11:04:51 +0200

    Fragments pasados a Activity
    Transformado el LangFragment a una Activity

--- MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/LangActivity.java
+++ MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/LangActivity.java
@@ -1,13 +1,49 @@
 package es.ugr.redforest.museumsforeveryone;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class LangActivity extends AppCompatActivity {
+
+	private ArrayList<Language> langList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lang);
+
+	    langList = new ArrayList<>();
+	    RecyclerView recyclerLang = (RecyclerView) findViewById(R.id.recycler_lang);
+	    LangAdapter langAdapter = new LangAdapter(langList);
+	    LinearLayoutManager layMan = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,
+															 false);
+
+	    recyclerLang.setLayoutManager(layMan);
+	    recyclerLang.setItemAnimator(new DefaultItemAnimator());
+	    recyclerLang.setAdapter(langAdapter);
+
+	    cargarIdiomas();    //SOLO PARA EL PROTOTIPO
+
     }
+
+	private void cargarIdiomas() {
+		Language es = new Language("Español", R.drawable.spain);
+		langList.add(es);
+
+		Language en = new Language("English", R.drawable.english);
+		langList.add(en);
+
+		Language fr = new Language("Français", R.drawable.french);
+		langList.add(fr);
+
+		Language de = new Language("Deutsch", R.drawable.german);
+		langList.add(de);
+	}
+
 }
