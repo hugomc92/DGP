493263ca38c76ca94afe2a5f306450ac5e8bb770
Hugo Maldonado <hugomc92@gmail.com>
2016-12-04 01:48:29 +0100

    Añadidas pestañas dinámicas para añadir contenido en idioma seleccionado

--- server/public/static/js/src/content_b.js
+++ server/public/static/js/src/content_b.js
@@ -1,5 +1,6 @@
 var currentLangs = [];
 var choosenAddLang;
+var langs;
 
 
 $(document).ready(function() {
@@ -17,6 +18,7 @@ $(document).ready(function() {
 			url: "/api/lang?email="+$("#email").text(),
 			datatype: "json",
 			success: function(jsondata) {
+				langs = jsondata;
 
 				var selectionHTML = '<select id="lang_select"><option value="" disabled selected>Elige un idioma</option>';
 
@@ -37,9 +39,13 @@ $(document).ready(function() {
 					$('#lang_selection').append(selectionHTML);
 
 					$('select').material_select();
+
+					$('#add_lang_submit').text('AÑADIR');
 				}
 				else {
 					$('#lang_selection').append('<h4>ESTE CONTENIDO YA ESTÁ EN TODOS LOS IDIOMAS DISPONIBLES</h4>');
+
+					$('#add_lang_submit').text('CERRAR');
 				}
 
 				$('#add_lang_loader').css('opacity', '0');
@@ -57,7 +63,28 @@ $(document).ready(function() {
 	$('#add_lang_submit').click(function() {
 		$('#add_lang_content').closeModal();
 
-		alert($('#lang_select').val());
+		if($('#lang_select').val() !== null) {
+			var langId = parseInt($('#lang_select').val());
+			
+			console.log("langID: ", langId);
+
+			currentLangs.push(langId);
+
+			for(var i=0; i<langs.length; i++) {
+				if(langs[i].ID === langId) {
+					// Add new Tab
+					$('.tabs').append('<li class="tab col"><a href="#' + langs[i].NAME.toLowerCase() + '_content" content-lang="' + langs[i].ID + '">' + langs[i].NAME + '</a></li>');
+
+					// Update all tabs
+					$('ul.tabs').tabs();
+
+					// Add new content tab
+					$('#contents').append('<div id="' + langs[i].NAME.toLowerCase() + '_content" class="col s12" style="display:none">Test 1</div>');
+				}
+			}
+			
+			console.log("currentLangs", currentLangs);
+		}
 
 		cleanAddLangModal();
 	});
@@ -69,5 +96,4 @@ function cleanAddLangModal() {
 
 	$('#add_lang_loader').css('opacity', '1');
 	$('#add_lang_loader').css('display', 'initial');
-
 }
