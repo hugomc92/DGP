7e05b105b716a0f0869b2a152c97ab6da77bb9b6
mrsasuu <mrsasuu@gmail.com>
2016-12-06 22:04:23 +0100

    Lector de QR implementado
    Aun falta por implementar bien la pantalla que redirige al lector y alguna cosilla.
    No funciona en Sony Xperia Z3 por el momento, en el resto de dispositivos si.

--- MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/utils/SliderMenu.java
+++ MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/utils/SliderMenu.java
@@ -1,9 +1,11 @@
 package es.ugr.redforest.museumsforeveryone.utils;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.RecyclerView;
