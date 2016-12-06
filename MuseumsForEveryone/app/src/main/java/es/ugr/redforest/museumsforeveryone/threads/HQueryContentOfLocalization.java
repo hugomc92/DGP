8d9497f33f8cb0778b9568ce6607e1c9da663671
nen155 <nen155@yahoo.es>
2016-12-06 18:55:54 +0100

    Añadido el QueyContentOfLocalization
    Falta por hacerlo en el server añadido un ejemplo de JSON: 
    https://drive.google.com/open?id=0BxQN-7UwLKtIaGhEUFo3RlF2LTg

--- MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/threads/HQueryContentOfLocalization.java
+++ MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/threads/HQueryContentOfLocalization.java
@@ -16,9 +16,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
 
+import es.ugr.redforest.museumsforeveryone.models.Content;
 import es.ugr.redforest.museumsforeveryone.models.ContentInformation;
+import es.ugr.redforest.museumsforeveryone.models.ContentType;
 import es.ugr.redforest.museumsforeveryone.models.Localization;
+import es.ugr.redforest.museumsforeveryone.models.Multimedia;
+import es.ugr.redforest.museumsforeveryone.utils.ControllerPreferences;
 import es.ugr.redforest.museumsforeveryone.utils.QueryBBDD;
 
 /**
@@ -49,17 +58,51 @@ public class HQueryContentOfLocalization extends AsyncTask<Void, Integer, String
         ObjectMapper mapper = new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         result = QueryBBDD.doQuery(QueryBBDD.queryContentOfLocalization +"/"+id+"/lang/"+language, "", "POST");
         JSONObject res =null;
-        ContentInformation itemContentInformation =null;
         try {
             if(result !=null) {
                 res = new JSONObject(result);
-                if (!res.isNull("ContentInformation")) {
-                    JSONArray contentType = res.getJSONArray("ContentInformation");
+                if (!res.isNull("localization")) {
+                    //Transform JSON localization to model localization
+                    JSONObject loc = res.getJSONObject("localization");
+                    localization =  mapper.readValue(loc.toString(), Localization.class);
 
-                    for (int j = 0; j < contentType.length(); ++j) {
-                        JSONObject item = contentType.getJSONObject(j);
-                        itemContentInformation = mapper.readValue(item.toString(), ContentInformation.class);
+                    ContentInformation itemContentInformation =null;
+                    ContentType itemContentType =null;
+                    Content content=null;
+                    //Fetch array of contents
+                    JSONArray contentsJSON = res.getJSONArray("contents");
+                    for (int j = 0; j < contentsJSON.length(); ++j) {
+                        JSONObject item = contentsJSON.getJSONObject(j);
+                        //Transform JSON content to model content
+                        JSONObject contentJson = item.getJSONObject("content");
+                        content =  mapper.readValue(contentJson.toString(), Content.class);
+                        //Transform JSON content infomration to model content infomration
+                        JSONObject content_information = item.getJSONObject("content_information");
+                        itemContentInformation = mapper.readValue(content_information.toString(), ContentInformation.class);
+                        //Transform JSON content type to model content type
+                        JSONObject content_type = item.getJSONObject("content_type");
+                        itemContentType = mapper.readValue(content_type.toString(), ContentType.class);
+                        //Add videos to content
+                        JSONArray videosJson = item.getJSONArray("video");
+                        for(int i=0;i<videosJson.length();++i) {
+                            JSONObject videoJson = videosJson.getJSONObject(i);
+                            Multimedia video = new Multimedia(videoJson.getInt("id"), videoJson.getString("url"), "video", videoJson.getString("subtitles"));
+                            content.addMultimedia(video);
+                        }
+                        //Add images to content
+                        JSONArray imagesJson = item.getJSONArray("images");
+                        for(int i=0;i<imagesJson.length();++i) {
+                            JSONObject imageJson = imagesJson.getJSONObject(i);
+                            Multimedia image = new Multimedia(imageJson.getInt("id"), imageJson.getString("url"), "image", imageJson.getString("alt_image"));
+                            content.addMultimedia(image);
+                        }
+                        //add content information and content type to content
+                        content.setContentInformation(itemContentInformation);
+                        content.setContentType(itemContentType);
+                        //add content to localization
+                        localization.addContent(content);
                     }
+
                 }
             }
         } catch (JSONException e) {
@@ -83,7 +126,7 @@ public class HQueryContentOfLocalization extends AsyncTask<Void, Integer, String
 
         if (resultado==null && resultado.compareTo("")==0) {
             Toast toast = Toast.makeText(context,
-                    "Problemas con internet",
+                    "Conection problems, try again",
                     Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
             toast.show();
