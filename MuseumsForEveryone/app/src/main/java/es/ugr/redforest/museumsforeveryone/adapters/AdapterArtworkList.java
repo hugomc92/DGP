ef9cae2bbdc251ae4eed0bc3e8cca9c2df002fd0
nen155 <nen155@yahoo.es>
2016-12-12 20:43:11 +0100

    Añadidas funcionalidades de listado de contenidos

--- MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/adapters/AdapterArtworkList.java
+++ MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/adapters/AdapterArtworkList.java
@@ -1,5 +1,6 @@
 package es.ugr.redforest.museumsforeveryone.adapters;
 
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 
 import es.ugr.redforest.museumsforeveryone.R;
 import es.ugr.redforest.museumsforeveryone.models.ContentInformation;
+import es.ugr.redforest.museumsforeveryone.models.Multimedia;
 
 /**
  * Adapter used in RecyclerView to display a list of Content information
@@ -22,8 +24,9 @@ import es.ugr.redforest.museumsforeveryone.models.ContentInformation;
  * @see RecyclerView
  */
 
-public class AdapterContentInformation extends RecyclerView.Adapter<AdapterContentInformation.ContentInformationViewHolder> {
+public class AdapterArtworkList extends RecyclerView.Adapter<AdapterArtworkList.ContentInformationViewHolder> {
     private ArrayList<ContentInformation> contentInformationList;   // ArrayList containing the Content Types to show
+    private ArrayList<Multimedia> images; //ArrayList imgaes to show
 
     /**
      * ViewHolder needed to handle how to show elements
@@ -34,6 +37,7 @@ public class AdapterContentInformation extends RecyclerView.Adapter<AdapterConte
      */
     public static class ContentInformationViewHolder extends RecyclerView.ViewHolder {
         private TextView contentInformationTxt;
+        private ImageView contentInformationImage;
 
         /**
          * Constructor method. Gets references to visible elements
@@ -42,7 +46,7 @@ public class AdapterContentInformation extends RecyclerView.Adapter<AdapterConte
          */
         public  ContentInformationViewHolder(View view) {
             super(view);
-
+            contentInformationImage = (ImageView) view.findViewById(R.id.rowIcon);
             contentInformationTxt = (TextView) view.findViewById(R.id.content_information_txt);
         }
     }
@@ -52,8 +56,9 @@ public class AdapterContentInformation extends RecyclerView.Adapter<AdapterConte
      *
      * @param contentInformationList List of languages to show
      */
-    public AdapterContentInformation(ArrayList<ContentInformation> contentInformationList) {
+    public AdapterArtworkList(ArrayList<ContentInformation> contentInformationList, ArrayList<Multimedia> images) {
         this.contentInformationList = contentInformationList;
+        this.images = images;
     }
 
     /**
@@ -74,6 +79,9 @@ public class AdapterContentInformation extends RecyclerView.Adapter<AdapterConte
         ContentInformation contentType = contentInformationList.get(position);
 
         holder.contentInformationTxt.setText(contentType.getName());
+        holder.contentInformationImage.setImageURI(Uri.parse(images.get(position).getUrl()));
+        holder.contentInformationImage.setContentDescription(images.get(position).getAlternativeText());
+
     }
 
     /**
--- MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/models/Location.java
+++ MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/models/Location.java
@@ -1,29 +1,37 @@
 package es.ugr.redforest.museumsforeveryone.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 
 /**
- * Class containing all data from a Localization
+ * Class containing all data from a Location
  *
  * @author Miguel Ángel Torres López
  * @author Emilio Chica Jiménez
  * @version 1.0.0
  */
 
-public class Localization {
+public class Location {
     private int id;
     private String description;
     private String nfc;
     private String qr;
     private String coordinates;
+    @JsonIgnoreProperties
     private ArrayList<Content> contents;
 
-    public Localization(int id, String description, String nfc, String qr, String coordinates) {
+    public Location(int id, String description, String nfc, String qr, String coordinates) {
         this.id = id;
         this.description = description;
         this.nfc = nfc;
         this.qr = qr;
         this.coordinates = coordinates;
+        this.contents = new ArrayList<>();
+    }
+    public Location(){
         this.contents = new ArrayList<>();
     }
 
@@ -33,42 +41,43 @@ public class Localization {
     public void addContent(Content c){
         contents.add(c);
     }
+    @JsonProperty("ID")
     public int getId() {
         return id;
     }
-
+    @JsonProperty("DESCRIPTION")
     public String getDescription() {
         return description;
     }
-
+    @JsonProperty("NFC")
     public String getNfc() {
         return nfc;
     }
-
+    @JsonProperty("QR")
     public String getQr() {
         return qr;
     }
-
+    @JsonProperty("COORDINATES")
     public String getCoordinates() {
         return coordinates;
     }
-
+    @JsonProperty("ID")
     public void setId(int id) {
         this.id = id;
     }
-
+    @JsonProperty("DESCRIPTION")
     public void setDescription(String description) {
         this.description = description;
     }
-
+    @JsonProperty("NFC")
     public void setNfc(String nfc) {
         this.nfc = nfc;
     }
-
+    @JsonProperty("QR")
     public void setQr(String qr) {
         this.qr = qr;
     }
-
+    @JsonProperty("COORDINATES")
     public void setCoordinates(String coordinates) {
         this.coordinates = coordinates;
     }
