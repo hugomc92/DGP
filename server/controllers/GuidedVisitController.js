95d6741f1f8b7e254a365253fec8d5343e85417f
Hugo Maldonado <hugomc92@gmail.com>
2016-12-17 18:53:33 +0100

    Fixes y lista de visitas

--- server/controllers/GuidedVisitController.js
+++ server/controllers/GuidedVisitController.js
@@ -5,8 +5,11 @@ var fs = require('fs');
 var path = require('path');
 
 var Utils = require('../utils/Util');
+var Language = require('../models/Language');
 var GuidedVisit = require('../models/GuidedVisit');
 var GuidedVisitInfo = require('../models/GuidedVisitInfo');
+var LocalizationVisit = require('../models/LocalizationVisit');
+var Localization = require('../models/Localization');
 
 // Constructor for ContentTypeController
 function GuidedVisitController(json, activityLogC, localizationC) {
@@ -29,13 +32,55 @@ GuidedVisitController.prototype.initBackend = function () {
 	var self = this;
 
 	self.routerBackend.route('/').get(function(req, res) {
-		self.renderJson.breadcrumb = {'LINK': '/backend/contentTypes/', 'SECTION': 'Visitas Guiadas'};
+		self.renderJson.breadcrumb = {'LINK': '/backend/guided_visits/', 'SECTION': 'Visitas Guiadas'};
 		self.renderJson.user = req.session.user;
 
 		if(typeof self.renderJson.user !== 'undefined' && parseInt(self.renderJson.user.ADMIN)) {
-			
-			res.render('pages/backend/guided_visits', self.renderJson);
-			self.clearMessages();
+			var guidedVisit = GuidedVisit.build();
+
+			guidedVisit.retrieveAll().then(function(success) {
+				var visits = success;
+
+				var visitIds = [];
+				for(var i=0; i<visits.length; i++)
+					visitIds.push(visits[i].ID);
+
+				var guidedVisitInfo = GuidedVisitInfo.build();
+
+				guidedVisitInfo.retrieveByVisitIdList(visitIds).then(function(success) {
+					var visitInfos = success;
+
+					var langIds = [];
+					for(var i=0; i<visitInfos.length; i++)
+						langIds.push(visitInfos[i].LANG_ID);
+
+					var language = Language.build();
+
+					language.retrieveAllByListIds(langIds).then(function(success) {
+						var langs = success;
+
+						self.renderJson.visits = visits;
+						self.renderJson.visitsInfos = visitInfos;
+						self.renderJson.langs = langs;
+
+						res.render('pages/backend/guided_visits', self.renderJson);
+						self.clearMessages();
+					}, function(err) {
+						self.renderJson.error = 'Error interno recuperando los idiomas de las visitas';
+
+						res.redirect('/backend/');
+					});
+				}, function(err) {
+					self.renderJson.error = 'Error interno recuperando la información de las visitas';
+
+					res.redirect('/backend/');
+				});
+				
+			}, function(err) {
+				self.renderJson.error = 'Error interno recuperando las visitas';
+
+				res.redirect('/backend/');
+			});
 		}
 		else {
 			res.redirect('/');
