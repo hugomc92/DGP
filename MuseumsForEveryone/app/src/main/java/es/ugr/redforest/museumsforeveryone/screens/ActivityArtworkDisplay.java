d7742fed614812e9d0b52e685fc6b9f2461ac3a8
nen155 <nen155@yahoo.es>
2017-01-11 19:58:02 +0100

    video carrusel, video se para cuando te vas y diversos añadidos

--- MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/screens/ActivityArtworkDisplay.java
+++ MuseumsForEveryone/app/src/main/java/es/ugr/redforest/museumsforeveryone/screens/ActivityArtworkDisplay.java
@@ -4,7 +4,16 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 
 import es.ugr.redforest.museumsforeveryone.R;
 import es.ugr.redforest.museumsforeveryone.models.Location;
@@ -36,6 +45,17 @@ public class ActivityArtworkDisplay extends AppCompatActivity {
         if(bundle.containsKey("index")){
             index = bundle.getInt("index");
         }
+        // 1. Create a default TrackSelector
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+        // 2. Create a default LoadControl
+        LoadControl loadControl = new DefaultLoadControl();
+        // 3. Create the player
+        player = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
+
         if(bundle.containsKey("qrornfc")) {
             id = bundle.getString("qrornfc");
             HQueryContentOfLocalization queryContent = new HQueryContentOfLocalization(this, id,index,mySlide,true,player);
